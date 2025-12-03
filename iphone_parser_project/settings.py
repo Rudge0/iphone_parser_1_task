0e88de512cbeb9813222fe3678e6ddf30d101ecd
Rudge0 <maksim.pariychuk07@gmail.com>
2025-12-03 01:05:04 +0100

    Clean project structure and add Django + parser files

--- iphone_parser_project/settings.py
+++ iphone_parser_project/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,17 +78,15 @@ WSGI_APPLICATION = 'iphone_parser_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'iphone_parser',
-        'USER': 'postgres',
-        'PASSWORD': 'Karavan1806',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
-
-
 
 
 # Password validation
